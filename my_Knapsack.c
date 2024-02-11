9358dddea2e3232b70a10c0a77e307686d33d0e8
NevoBiton20 <155353129+NevoBiton20@users.noreply.github.com>
2024-02-11 18:58:33 +0200

    Create my_Knapsack.c

---
+++ my_Knapsack.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+
+#define ITEMS 5
+#define Mweigth 20
+#define TRUE 1
+#define FALSE 0
+
+int result[ITEMS];
+
+int max(int num,int num2)
+{
+    return num>num2 ? num : num2;
+}
+
+
+int knapSack(int weigths[], int values[], int selected_bool[])
+{
+    int table[ITEMS +1][Mweigth+1];
+
+    for(int j=0;j<=ITEMS;j++)
+    {
+        for(int w=0;w<=Mweigth;w++)
+        {
+            if(j==0 || w==0)
+            {
+                table[j][w]=0;
+            }
+            else
+            {
+                if(weigths[j-1]<=w)
+                {
+                    table[j][w] = max(table[j-1][w], table[j-1][w-weigths[j-1]] + values[j-1]);
+                }
+                else{
+                    table[j][w] = table[j-1][w];
+                }
+            }
+        }
+    }
+    int j=ITEMS, w = Mweigth;
+    while(j > 0 && w > 0)
+    {
+        if(table[j][w] != table[j-1][w])
+        {
+            selected_bool[j-1]=1;
+            w-= weigths[j-1];
+        }
+        j--;
+    }
+    return table[ITEMS][Mweigth];
+}
+
+
+int main()
+{
+    int values[ITEMS];
+    int weights[ITEMS];
+
+    char item =0;
+
+    for(int i=0; i<ITEMS ;i++)
+    {
+        scanf("%c",&item);
+
+        switch (item)
+        {
+            case 'a':
+                scanf("%d",&values[0]);
+                scanf("%d",&weights[0]);
+                break;
+            case 'b':
+                scanf("%d",&values[1]);
+                scanf("%d",&weights[1]);
+                
+                break;
+            case 'c':
+                scanf("%d",&values[2]);
+                scanf("%d",&weights[2]);
+                break;
+            case 'd':
+                scanf("%d",&values[3]);
+                scanf("%d",&weights[3]);
+                break;
+            case 'e':
+                scanf("%d",&values[4]);
+                scanf("%d",&weights[4]);
+                break;
+        }
+    }
+    int selected[ITEMS];
+    int maxProfit = knapSack(weights,values,selected);
+    printf("Maximum profit: %d\n",maxProfit);
+    printf("Items that give the maximum profit:");
+    for(int i=0;i<ITEMS;i++)
+    {
+        if(selected[i]==TRUE)
+        {
+            printf(" %c",selected[i]);
+        }
+    }
+    return 0;
+}
